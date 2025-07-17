924969af3cc7f66d63a96a674ba93366dd6196c1
riteshco <riteshgadwalv2@gmail.com>
2025-07-17 16:29:59 +0530

    Add initial two rays for vertical and horizontal lines tracking each respectively
    Signed-off-by: riteshco <riteshgadwalv2@gmail.com>

--- main.c
+++ main.c
@@ -1,14 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#include <math.h>
+#define PI 3.1415926535
+#define P2 PI/2
+#define P3 3*PI/2
 
-float px , py; //player position
+float px,py,pdx,pdy,pa; //player position
 
 void drawPlayer(){
     glColor3f(1 , 1 , 0);
     glPointSize(8);
     glBegin(GL_POINTS);
     glVertex2i(px , py);
+    glEnd();
+
+    glLineWidth(3);
+    glBegin(GL_LINES);
+    glVertex2i(px , py);
+    glVertex2i(px+pdx*5 , py+pdy*5);
     glEnd();
 }
 
@@ -26,18 +36,62 @@ int map[] =
 };
 
 void drawMap2D(){
-    int x , y , x0 , y0;
+    int x , y , xo , yo;
     for ( y=0 ; y<mapY ; y++){
         for(x=0;x<mapX;x++){
             if(map[y*mapX+x]==1){glColor3f(1,1,1);} else { glColor3f(0,0,0); }
-            x0 = x*mapS; y0 = y*mapS;
+            xo = x*mapS; yo = y*mapS;
             glBegin(GL_QUADS);
-            glVertex2i(x0     +1, y0     +1);
-            glVertex2i(x0     +1, y0+mapS-1);
-            glVertex2i(x0+mapS-1, y0+mapS-1);
-            glVertex2i(x0+mapS-1, y0     +1);
+            glVertex2i(xo     +1, yo     +1);
+            glVertex2i(xo     +1, yo+mapS-1);
+            glVertex2i(xo+mapS-1, yo+mapS-1);
+            glVertex2i(xo+mapS-1, yo     +1);
             glEnd();
         }
+    }
+}
+
+void drawRays3D(){
+    int r , mx , my , mp, dof; float rx,ry,ra,xo,yo;
+    ra=pa;
+    for(r=0;r<1;r++){
+        // --Check Horizontal Lines--
+        dof=0;
+        float aTan=-1/tan(ra);
+        if(ra > PI){ ry=(((int)py >> 6) << 6)-0.0001; rx=(py-ry)*aTan+px; yo=-64 ; xo=-yo*aTan;}//looking up
+        if(ra < PI){ ry=(((int)py >> 6) << 6)+64    ; rx=(py-ry)*aTan+px; yo= 64 ; xo=-yo*aTan;}//looking down
+        if(ra==0 || ra==PI){ rx=px; ry=py; dof=8;}//looking straight left or right
+        while(dof<8)
+        {
+            mx=(int) (rx)>>6; my=(int)(ry) >> 6; mp=my*mapX+mx;
+            if(mp>0 && mp<mapX*mapY && map[mp]==1){dof = 8;} //hit wall
+            else{ rx+=xo; ry+=yo; dof+=1;}//next line
+        }
+        glColor3f(0,1,0);
+        glLineWidth(10);
+        glBegin(GL_LINES);
+        glVertex2i(px,py);
+        glVertex2d(rx,ry);
+        glEnd();
+
+        //--Check Vertical Lines--
+        dof=0;
+        float nTan=-tan(ra);
+        if(ra > P2 && ra<P3){ rx=(((int)px >> 6) << 6)-0.0001; ry=(px-rx)*nTan+py; xo=-64 ; yo=-xo*nTan;}//looking left
+        if(ra < P2 || ra>P3){ rx=(((int)px >> 6) << 6)+64    ; ry=(px-rx)*nTan+py; xo= 64 ; yo=-xo*nTan;}//looking right
+        if(ra==P2 || ra==P3){ rx=px; ry=py; dof=8;}//looking straight up or down
+        while(dof<8)
+        {
+            mx=(int) (rx)>>6; my=(int)(ry) >> 6; mp=my*mapX+mx;
+            if(mp>0 && mp<mapX*mapY && map[mp]==1){dof = 8;} //hit wall
+            else{ rx+=xo; ry+=yo; dof+=1;}//next line
+        }
+        glColor3f(1,0,0);
+        glLineWidth(3);
+        glBegin(GL_LINES);
+        glVertex2i(px,py);
+        glVertex2d(rx,ry);
+        glEnd();
     }
 }
 
@@ -45,21 +99,22 @@ void display(){
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     drawMap2D();
     drawPlayer();
+    drawRays3D();
     glutSwapBuffers();
 }
 
 void buttons(unsigned char key , int x,int y){
-    if(key == 'a'){px -=5;}
-    if(key == 'd'){px +=5;}
-    if(key == 's'){py +=5;}
-    if(key == 'w'){py -=5;}
+    if(key == 'a'){pa -= 0.1; if (pa <    0){ pa+=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+    if(key == 'd'){pa += 0.1; if (pa > 2*PI){ pa-=2*PI;} pdx=cos(pa)*5; pdy=sin(pa)*5;}
+    if(key == 's'){ px-= pdx; py-=pdy;}
+    if(key == 'w'){ px+= pdx; py+=pdy;}
     glutPostRedisplay();
 }
 
 void init(){
     glClearColor(0.3 , 0.3 , 0.3 , 0);
     gluOrtho2D(0, 1024 , 512 , 0);
-    px = 300 ; py = 300;
+    px = 300 ; py = 300; pdx=cos(pa)*5; pdy=sin(pa)*5;
 }
 
 int main(int argc, char* argv[]){
